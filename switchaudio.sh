55727485a358d4089051cf8265d3ead46cac0dbc
Syamir <bleach707@gmail.com>
2022-09-30 17:34:12 +0800

    -updated comments

--- switchaudio.sh
+++ switchaudio.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/zsh
 # This script uses dunstify notification to notify the user of the current profile.
-#Identify your headphones and speaker sources and add them to the script
+#Identify your headphones and speaker sinks and add them to the script
 
 CURRENT_PROFILE=$(pactl get-default-sink | tr -d '\n') 
 HEADPHONE="alsa_output.usb-FiiO_DigiHug_USB_Audio-01.analog-stereo"
